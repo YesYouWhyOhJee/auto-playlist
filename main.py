56dc45efc1ce40f0fabcf07c360590aa749a175c
YesYouWhyOhJee <suyog45work@gmail.com>
2021-09-20 10:15:49 +0530

    second commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 from googleapiclient.discovery import build
 import google_auth_oauthlib.flow
 import googleapiclient.errors
+import logging
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 client_secrets_file = 'youtube-api-key.json'
@@ -12,7 +13,13 @@ credentials = flow.run_console()
 yt = build('youtube', 'v3', credentials=credentials)
 
 
-request = yt.playlists().list(part='snippet,contentDetails',maxResults=5,mine=True)
+request = yt.playlists().list(part='snippet',maxResults=5,mine=True)
 response = request.execute()
+logging.basicConfig(filename='log.txt', encoding='utf-8', level=logging.DEBUG)
+f = open('response.json', 'a')
+f.write(json.dumps(response))
+f.close()
+logging.info(response)
+
 
 
